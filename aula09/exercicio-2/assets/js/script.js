12c418b078388ed6a0c0afa018329249a815a592
Livia G <68361379+4L1C3-R4BB1T@users.noreply.github.com>
2020-08-04 22:11:43 -0300

    Exercícios - Armazenando dados de Forms em variáveis

--- aula09/exercicio-2/assets/js/script.js
+++ aula09/exercicio-2/assets/js/script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
  var nome = form1.nome.value;
  var sobrenome = form1.sobrenome.value;
 
- alert('Nome: ' + nome + " \nSobrenome: " + sobrenome);
+ alert('Nome: ' + nome + ' \nSobrenome: ' + sobrenome);
 }
