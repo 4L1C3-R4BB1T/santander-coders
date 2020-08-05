258261e1437c75c1c0dc68e30db59a56f891cd0a
Livia <livinha.guimaraes.013@gmail.com>
2020-08-04 22:04:58 -0300

    Exercícios - Armazenando dados de Forms em variáveis

---
+++ aula09/exercicio-2/README.md
@@ -0,0 +1,5 @@
+# Curso Full Stack Java - 03/08/2020
+<ul>
+  <li>Armazenando dados de Formulários em variáveis</li>
+  <li>Validação de Formulários</li>
+</ul>
---
+++ aula09/exercicio-2/assets/js/script.js
@@ -0,0 +1,6 @@
+function getFormvalue() {
+ var nome = form1.nome.value;
+ var sobrenome = form1.sobrenome.value;
+
+ alert('Nome: ' + nome + " \nSobrenome: " + sobrenome);
+}
