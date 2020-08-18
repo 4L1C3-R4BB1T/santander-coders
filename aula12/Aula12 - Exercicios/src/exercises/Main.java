873e8bc5a6ed6723b1944bf5ce1c15f11cdc3998
Livia <68361379+4L1C3-R4BB1T@users.noreply.github.com>
2020-08-17 22:32:27 -0300

    Fundamentos da Programação - Java

--- aula12/Aula12 - Exercicios/src/exercises/Main.java
+++ aula12/Aula12 - Exercicios/src/exercises/Main.java
@@ -9,40 +9,40 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		int idade;
 		
-		//4-recebe um nome e imprime uma saudação
+		// 4-recebe um nome e imprime uma saudaÃ§Ã£o
 		
 		System.out.println("Digite seu nome:");
 		String nome = input.next();
 		
-		System.out.println("Olá " + nome + "!");
+		System.out.println("OlÃ¡ " + nome + "!");
 	
 		
-		//5-recebe uma idade e retorna se é maior ou menor de idade
+		// 5-recebe uma idade e retorna se Ã© maior ou menor de idade
 		
 		System.out.println("Digite sua idade:");
 		idade = input.nextInt();
 		
 		if(idade < 18) {
-			System.out.println("Você é menor de idade.");
+			System.out.println("VocÃª Ã© menor de idade.");
 		}else {
-			System.out.println("Você é maior de idade.");
+			System.out.println("VocÃª Ã© maior de idade.");
 		}
 		
 		
-		//6-dado um dia da semana, verifica se é final de semana
+		// 6-dado um dia da semana, verifica se Ã© final de semana
 		
 		System.out.println("Digite o dia da semana:");
 		String diaSemana = input.next();
 		
-		if((diaSemana.equalsIgnoreCase("sabado")) || (diaSemana.equalsIgnoreCase("sábado")) 
+		if((diaSemana.equalsIgnoreCase("sabado")) || (diaSemana.equalsIgnoreCase("sÃ¡bado")) 
 				|| (diaSemana.equalsIgnoreCase("domingo"))) {
-			System.out.println("É final de semana! :)");
+			System.out.println("Ã‰ final de semana! :)");
 		}else {
-			System.out.println("Não é final de semana! :(");
+			System.out.println("NÃ£o Ã© final de semana! :(");
 		}
 		
 		
-		//7-crie um programa que verifique se a pessoa tem todos os requisitos para dirigir
+		// 7-crie um programa que verifique se a pessoa tem todos os requisitos para dirigir
 		
 		System.out.println("Digite sua idade:");
 		idade = input.nextInt();
@@ -51,9 +51,9 @@ public class Main {
 		String possuiCnh = input.next();
 		
 		if((idade >= 18) && (possuiCnh.equalsIgnoreCase("s"))) {
-			System.out.println("Você pode dirigir!");
+			System.out.println("VocÃª pode dirigir!");
 		}else {
-			System.out.println("Você não pode dirigir!");
+			System.out.println("VocÃª nÃ£o pode dirigir!");
 		}
 			
 	}
